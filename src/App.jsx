037b8021a06867cb7a2a7c4fa8dc27690d67fbf9
The Angry Coder <79235452+TheAngryCoder2@users.noreply.github.com>
2025-01-03 21:55:37 +0000

    wip

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Logo from './assets/Untitled.webp'
+
+export default function App() {
+  return (
+    // Header
+    <div className="flex w-full">
+      <img src={Logo} width={200} className='m-5' />
+    </div>
+  )
+}
